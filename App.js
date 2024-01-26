68a9a3501c537ef3553325fce0f0d2340bdcf55a
Antoine Cottineau <antoine.cottineau.dev@gmail.com>
2024-01-26 17:00:25 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:App.js

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View, } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Pressable onPress={() => {
+        bench
+      }}>
+        <Text>Open up App.js to start working on your app!</Text></Pressable>
       <StatusBar style="auto" />
     </View>
   );
